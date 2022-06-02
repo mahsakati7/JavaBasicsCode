353f61856d155eb89656eb9c8a69d89f69bc6f6c
mahsakati7 <mahsakadkhodaei@Mahyars-MacBook-Air.local>
2022-06-01 17:06:13 -0700

    contains 8 java classes

---
+++ src/com/syntax/class01/MyFirstProgram.java
@@ -0,0 +1,7 @@
+package com.syntax.class01;
+
+public class MyFirstProgram {
+	public static void main(String[] args) {
+
+		
+		
---
+++ src/com/syntax/class01/ThirdProgram.java
@@ -0,0 +1,14 @@
+package com.syntax.class01;
+
+public class ThirdProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("today we learn github");
+	
+		System.out.println("We love java");
+	}
+	
+
+}
